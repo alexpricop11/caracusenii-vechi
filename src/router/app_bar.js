b5bc4f12ee12165711d8b7f99f1d73313fe2345a
Smart <apricop05@gmail.com>
2024-11-14 19:54:08 +0200

    fixed route

--- src/router/app_bar.js
+++ src/router/app_bar.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 import Home from '../views/Home.vue';
 import School from '../views/School.vue';
 import Location from "@/views/Location.vue";
@@ -6,16 +6,17 @@ import History from "@/views/History.vue"
 import About from "@/views/About.vue";
 
 const routes = [
-  { path: '/caracusenii-vechi/', component: Home },
-  { path: '/school', component: School },
-  { path: '/location', component: Location },
-  { path: '/history', component: History },
-  { path: '/about', component: About },
+    {path: '/caracusenii-vechi/', component: Home},
+    {path: '/school', component: School},
+    {path: '/location', component: Location},
+    {path: '/history', component: History},
+    {path: '/about', component: About},
+    {path: '/:pathMatch(.*)*', redirect: '/caracusenii-vechi/'},
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+    history: createWebHistory(),
+    routes
 });
 
 export default router;
