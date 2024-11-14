7cd88ed0349a26e79bbf7de7468a11f69054c125
Smart <apricop05@gmail.com>
2024-11-14 22:12:51 +0200

    add 404.html

--- src/router/app_bar.js
+++ src/router/app_bar.js
@@ -4,6 +4,7 @@ import School from '../views/School.vue';
 import Location from "@/views/Location.vue";
 import History from "@/views/History.vue"
 import About from "@/views/About.vue";
+import NotFound from "@/views/NotFound.vue"; // Create this component for your 404 page
 
 const routes = [
     {path: '/caracusenii-vechi/', component: Home},
@@ -11,7 +12,7 @@ const routes = [
     {path: '/location', component: Location},
     {path: '/history', component: History},
     {path: '/about', component: About},
-    {path: '/:pathMatch(.*)*', redirect: '/caracusenii-vechi/'},
+    {path: '/:pathMatch(.*)*', component: NotFound}, // Catch-all route for 404
 ];
 
 const router = createRouter({
