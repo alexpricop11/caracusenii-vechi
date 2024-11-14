10c63e30ceb9d766bc4f97b2e4fc19bd59723131
Smart <apricop05@gmail.com>
2024-11-14 22:13:08 +0200

    add NotFound.vue

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>404 - Pagina nu a fost găsită</h1>
+    <p>Veți fi redirecționat către pagina principală în câteva secunde...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    setTimeout(() => {
+      this.$router.push('/caracusenii-vechi/');
+    }, 1000);
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
