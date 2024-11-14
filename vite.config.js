dbb96a86cb5c9156bc5eb1c02809036d08b05d33
Smart <apricop05@gmail.com>
2024-11-14 18:09:08 +0200

    update config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-    publicPath: '/caracusenii-vechi',
+    base: '/caracusenii-vechi',
     plugins: [
         vue(),
     ],
